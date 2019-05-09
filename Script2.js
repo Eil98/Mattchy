dde66b2a2194d57d4e785649606665f484e3fe10
Eil98 <48719646+Eil98@users.noreply.github.com>
2019-05-09 16:18:29 +0200

    Problema menu

--- Script2.js
+++ Script2.js
@@ -1,39 +1,42 @@
 $(document).ready(function () {
-
+    console.log("pronto");
     //Navbar-responsive
 
-    $("#icon").click(function() {
+    $("#hamb").click(function() {
+
         $("ul").slideToggle();
         $("ul ul").css("display", "none");
     });
 
     $("ul li").click(function() {
-        $("ul ul").slideUp();
         $(this).find('ul').slideToggle();
     });
+
+
 
     $(window).resize(function() {
         if($(window).width() > 768) {
             $("ul").removeAttr('style');
+            // $("#logoDex").css("display","none");
         }
     });
 
-            var prevScrollPos = 0;
+    var prevScrollPos = 0;
 
-            $(window).scroll(function () {
+    $(window).scroll(function () {
 
-                var currentScrollPos=$(window).scrollTop();
-                console.log(currentScrollPos + " + " + prevScrollPos);
-                if(currentScrollPos>prevScrollPos){
-                    //var isDown=currentScrollPos>prevScrollPos;
-                    console.log("CIAOO");
-                    $("#navbar").addClass("hidden");
-                }else if ( currentScrollPos<prevScrollPos ){
-                    $("#navbar").removeClass("hidden");
-                }
-                prevScrollPos=currentScrollPos;
+        var currentScrollPos=$(window).scrollTop();
+        console.log(currentScrollPos + " + " + prevScrollPos);
+        if(currentScrollPos>prevScrollPos){
+            //var isDown=currentScrollPos>prevScrollPos;
+            console.log("CooooO");
+            $("#navbar").addClass("hidden");
+        }else if ( currentScrollPos<prevScrollPos ){
+            $("#navbar").removeClass("hidden");
+        }
+        prevScrollPos=currentScrollPos;
 
-            });
+    });
 
 
 });
