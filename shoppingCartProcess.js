199dcc597bf8f2f71137d3fd819740a73964d949
Eil98 <48719646+Eil98@users.noreply.github.com>
2019-10-13 17:29:08 +0200

    Delete shoppingCartProcess.js

--- shoppingCartProcess.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-    $('#shopCartTrigg').click(function () {
-        if(localStorage.getItem('access')==null){
-            $("#loginOverlay").css("display","block");
-        }
-        
-    });
-});
